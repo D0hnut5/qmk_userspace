206206d204cde6668c1d0a036496f419f0d9c570
D0hnut5 <wmmillar@gmail.com>
2025-02-01 22:01:42 -0600

    modified:   keyboards/jlw/bruce_the_keyboard/keymaps/wildo/keymap.c 	keyboards/jlw/bruce_the_keyboard/keymaps/wildo/rules.mk 	users/wildo/

--- keyboards/jlw/bruce_the_keyboard/keymaps/wildo/keymap.c
+++ keyboards/jlw/bruce_the_keyboard/keymaps/wildo/keymap.c
@@ -2,34 +2,50 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-
+#include "wildo.h"
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-         KC_Q,          KC_W,           KC_F,           KC_P,   KC_B,    KC_J,   KC_L,   KC_U,           KC_Y,           KC_QUOT,
-         LGUI_T(KC_A),  LALT_T(KC_R),   LCTL_T(KC_S),   KC_T,   KC_G,    KC_M,   KC_N,   RCTL_T(KC_E),   LALT_T(KC_I),   LGUI_T(KC_O),
-         KC_Z,          KC_X,           KC_C,           KC_D,   KC_V,    KC_K,   KC_H,   KC_COMM,        KC_DOT,         KC_SLSH,
-                                        LT(3, KC_TAB), SFT_T(KC_BSPC),   LT(1, KC_SPC),  LT(2,KC_ENT)
+    [_BASE] = LAYOUT_wildo(
+          ___BASE_1_L___,                              ___BASE_1_R___,
+          ___BASE_2_L___,                              ___BASE_2_R___,
+          ___BASE_3_L___,                              ___BASE_3_R___,
+               ___BASE_2THM_L___,            ___BASE_2THM_L___  
     ),
-
-    [1] = LAYOUT(
-         KC_GRAVE, KC_F2,    XXXXXXX,  KC_F4,    KC_F5,    KC_BSLS,  KC_MINUS, KC_EQUAL, KC_LBRC,  KC_RBRC,
-         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-         DF(0),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  DF(1),    XXXXXXX,  _______,  _______,  KC_SCLN,
-                                       _______,  _______,  _______,  _______
+    [_NUM] = LAYOUT_wildo(
+          ___NUM_1_L___,                               ___NUM_1_R___,
+          ___NUM_2_L___,                               ___NUM_2_R___,
+          ___NUM_3_L___,                               ___NUM_3_R___,
+               ___NUM_2THM_L___,             ___NUM_2THM_L___  
+     ),
+    [_NAV] = LAYOUT_wildo(
+          ___NAV_1_L___,                               ___NAV_1_R___,
+          ___NAV_2_L___,                               ___NAV_2_R___,
+          ___NAV_3_L___,                               ___NAV_3_R___,
+               ___NAV_2THM_L___,             ___NAV_2THM_L___  
     ),
-
-    [2] = LAYOUT(
-         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,
-         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
-         DF(0),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  DF(2),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                       _______,  _______,  _______,  _______
+    [_MSE] = LAYOUT_wildo(
+          ___MSE_1_L___,                               ___MSE_1_R___,
+          ___MSE_2_L___,                               ___MSE_2_R___,
+          ___MSE_3_L___,                               ___MSE_3_R___,
+               ___MSE_2THM_L___,             ___MSE_2THM_L___  
     ),
-    [3] = LAYOUT(
-         XXXXXXX,  XXXXXXX,  LSG(KC_4),LSG(KC_S),XXXXXXX,  KC_HOME,  XXXXXXX,  KC_UP,    XXXXXXX,  KC_PGUP,
-         KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LGUI,  XXXXXXX,  KC_END,   KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PGDN,
-         DF(0),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  DF(2),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                       _______,  _______,  _______,  _______
+    [_FUN] = LAYOUT_wildo(
+          ___FUN_1_L___,                               ___FUN_1_R___,
+          ___FUN_2_L___,                               ___FUN_2_R___,
+          ___FUN_3_L___,                               ___FUN_3_R___,
+               ___FUN_2THM_L___,             ___FUN_2THM_L___  
+    ),
+    [_SYM] = LAYOUT_wildo(
+          ___SYM_1_L___,                               ___SYM_1_R___,
+          ___SYM_2_L___,                               ___SYM_2_R___,
+          ___SYM_3_L___,                               ___SYM_3_R___,
+               ___SYM_2THM_L___,             ___SYM_2THM_L___  
+    ),
+    [_ADD] = LAYOUT_wildo(
+          ___ADD_1_L___,                               ___ADD_1_R___,
+          ___ADD_2_L___,                               ___ADD_2_R___,
+          ___ADD_3_L___,                               ___ADD_3_R___,
+               ___ADD_2THM_L___,             ___ADD_2THM_L___  
     )
 };
