560130dafa6c4fe2d764faddd847181bcab27859
D0hnut5 <wmmillar@gmail.com>
2025-02-02 13:49:23 -0600

    modified:   keyboards/jlw/bruce_the_keyboard/keymaps/wildo/keymap.c 	deleted:    layouts/.keep 	modified:   qmk.json 	modified:   users/wildo/wildo.h 	keyboard_defs/ 	keyboards/jlw/vault35_katana/ 	keyboards/projectcain/ 	keyboards/sporkus/ 	users/wildo/combos_30alpha.h 	users/wildo/combos_def.h 	users/wildo/config.h 	users/wildo/encoder_map.h 	users/wildo/encoder_press.h

--- keyboards/jlw/bruce_the_keyboard/keymaps/wildo/keymap.c
+++ keyboards/jlw/bruce_the_keyboard/keymaps/wildo/keymap.c
@@ -10,42 +10,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           ___BASE_1_L___,                              ___BASE_1_R___,
           ___BASE_2_L___,                              ___BASE_2_R___,
           ___BASE_3_L___,                              ___BASE_3_R___,
-               ___BASE_2THM_L___,            ___BASE_2THM_L___  
+               ___BASE_2THM_L___,            ___BASE_2THM_R___  
     ),
     [_NUM] = LAYOUT_wildo(
           ___NUM_1_L___,                               ___NUM_1_R___,
           ___NUM_2_L___,                               ___NUM_2_R___,
           ___NUM_3_L___,                               ___NUM_3_R___,
-               ___NUM_2THM_L___,             ___NUM_2THM_L___  
-     ),
+               ___NUM_2THM_L___,             ___NUM_2THM_R___  
+    ),
     [_NAV] = LAYOUT_wildo(
           ___NAV_1_L___,                               ___NAV_1_R___,
           ___NAV_2_L___,                               ___NAV_2_R___,
           ___NAV_3_L___,                               ___NAV_3_R___,
-               ___NAV_2THM_L___,             ___NAV_2THM_L___  
+               ___NAV_2THM_L___,             ___NAV_2THM_R___  
     ),
     [_MSE] = LAYOUT_wildo(
           ___MSE_1_L___,                               ___MSE_1_R___,
           ___MSE_2_L___,                               ___MSE_2_R___,
           ___MSE_3_L___,                               ___MSE_3_R___,
-               ___MSE_2THM_L___,             ___MSE_2THM_L___  
+               ___MSE_2THM_L___,             ___MSE_2THM_R___  
     ),
     [_FUN] = LAYOUT_wildo(
           ___FUN_1_L___,                               ___FUN_1_R___,
           ___FUN_2_L___,                               ___FUN_2_R___,
           ___FUN_3_L___,                               ___FUN_3_R___,
-               ___FUN_2THM_L___,             ___FUN_2THM_L___  
+               ___FUN_2THM_L___,             ___FUN_2THM_R___  
     ),
     [_SYM] = LAYOUT_wildo(
           ___SYM_1_L___,                               ___SYM_1_R___,
           ___SYM_2_L___,                               ___SYM_2_R___,
           ___SYM_3_L___,                               ___SYM_3_R___,
-               ___SYM_2THM_L___,             ___SYM_2THM_L___  
+               ___SYM_2THM_L___,             ___SYM_2THM_R___  
     ),
     [_ADD] = LAYOUT_wildo(
           ___ADD_1_L___,                               ___ADD_1_R___,
           ___ADD_2_L___,                               ___ADD_2_R___,
           ___ADD_3_L___,                               ___ADD_3_R___,
-               ___ADD_2THM_L___,             ___ADD_2THM_L___  
+               ___ADD_2THM_L___,             ___ADD_2THM_R___  
     )
 };
--- users/wildo/wildo.h
+++ users/wildo/wildo.h
@@ -194,6 +194,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 */
 
+/*
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
@@ -203,6 +204,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [4] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
+*/
 
 /*#ifdef PERMISSIVE_HOLD_PER_KEY
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
@@ -258,8 +260,8 @@ combo_t key_combos[] = {
 #endif
 
 /** layout macros **/
-#define LAYOUT_wildo(...)                   LAYOUT(__VA_ARGS__)
-#define LAYOUT_wildo_split_6bars(...)       LAYOUT_split_6bars(__VA_ARGS_) 
+#define LAYOUT_wildo(...)              LAYOUT(__VA_ARGS__)
+#define LAYOUT_wildo_vault_katana(...) LAYOUT_split_6bars(__VA_ARGS__)
 /* portable keymaps end */
 
 #endif // USERSPACE
